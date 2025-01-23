d805ee2d7c3f8969e217c39287becc02cd23571b
ToniVP <tovivo73@gmail.com>
2025-01-23 12:03:22 +0100

    Creació arxiu dades netes per exploració de la sèrie temporal de fenologia per C. mediterranea

--- Serie_temporal.R
+++ Serie_temporal.R
@@ -14,14 +14,16 @@ library(readxl)
 cmed<-read.csv("./raw_data/temporal_series_C.mediterranea.txt",header=T,dec=".",sep="\t", check.names = F); 
 str(cmed);
 
-#create a new, clean big dataframe to work with
-data <- cmed %>%  
+#Data prep: create a new, clean big dataframe to work with
+cmed <- cmed %>%  
   mutate(Leave_size = as.numeric(gsub(",", ".", Leave_size)), Quadrat = as.factor(as.character(Quadrat)),
          Cauloide_size = as.numeric(gsub(",", ".", Cauloide_size))) %>% group_by(Population, Species, Date, Quadrat) %>% 
-  
+  summarise(caul_size = mean(Cauloide_size, na.rm = T), leaves_size = mean(Leave_size, na.rm = T), .groups = "drop_last")
 
-#Select specific species and year
-data = data %>% filter(Year == 2021)
+#write.table(cmed, "C.med_clean_temporal_phenology.txt", row.names = T,  sep="\t")
+
+#Data exploration: calculate different indices representing phenology --------
+data<-read.csv("C.med_clean_temporal_phenology.txt",header=T,dec=".",sep="\t", check.names = F)
 
 
 
